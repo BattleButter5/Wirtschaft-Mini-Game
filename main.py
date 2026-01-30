e527eb868a57db52211dba117f3914526e1e0684
Noah <naoh@silveira-family.de>
2026-01-30 16:46:14 +0100

    initial commit

--- main.py
+++ main.py
@@ -11,6 +11,8 @@ BG = pygame.transform.scale(pygame.image.load("bg.jpeg"), (WIDTH, HEIGHT))
 
 PLAYER_WIDTH = 40
 PLAYER_HEIGHT = 60
+TRUMP_WIDTH = 50
+TRUMP_HEIGHT = 70
 
 PLAYER_VEL = 5
 STAR_WIDTH = 10
@@ -21,16 +23,18 @@ STAR_VEL = 3
 FONT = pygame.font.SysFont("comicsans", 30)
 
 
-def draw(player, elapsed_time, stars):
+def draw(player, elapsed_time, stars, trump):
     WIN.blit(BG, (0, 0))
 
     time_text = FONT.render(f"Time: {round(elapsed_time)}s", 1, "white")
     WIN.blit(time_text, (10, 10))
 
     pygame.draw.rect(WIN, "red", player)
+    pygame.draw.rect(WIN, "blue", trump)
 
     for star in stars:
         pygame.draw.rect(WIN, "white", star)
+
 
     pygame.display.update()
 
@@ -38,9 +42,9 @@ def draw(player, elapsed_time, stars):
 def main():
     run = True
 
-    player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT,
-                         PLAYER_WIDTH, PLAYER_HEIGHT)
     player = pygame.Rect(200, HEIGHT - PLAYER_HEIGHT, PLAYER_WIDTH, PLAYER_HEIGHT)
+    trump = pygame.Rect(200, 5, TRUMP_WIDTH, TRUMP_HEIGHT )
+    TRUMP_VEL = random.randint(-5, 5)
 
     player_vel_y = 0  # vertical speed
     gravity = 0.5  # gravity pulling the player down
@@ -50,6 +54,7 @@ def main():
     clock = pygame.time.Clock()
     start_time = time.time()
     elapsed_time = 0
+    direction_timer = 0
 
     star_add_increment = 2000
     star_count = 0
@@ -60,11 +65,25 @@ def main():
     while run:
         star_count += clock.tick(60)
         elapsed_time = time.time() - start_time
+        direction_timer += clock.get_time()
+
+        if direction_timer > 1000:  # every 2 seconds
+            TRUMP_VEL = random.choice([-7, -5, 5, 7])
+            direction_timer = 0
+
+        trump.x += TRUMP_VEL
+
+        if trump.x <= 0 or trump.x + TRUMP_WIDTH >= WIDTH:
+                TRUMP_VEL *= -1
+
+
+
 
         if star_count > star_add_increment:
             for _ in range(3):
                 star_x = random.randint(0, WIDTH - STAR_WIDTH)
-                star = pygame.Rect(star_x, -STAR_HEIGHT,
+                star_y = TRUMP_HEIGHT
+                star = pygame.Rect(star_x, star_y,
                                    STAR_WIDTH, STAR_HEIGHT)
                 stars.append(star)
 
@@ -109,7 +128,7 @@ def main():
             pygame.time.delay(4000)
             break
 
-        draw(player, elapsed_time, stars)
+        draw(player, elapsed_time, stars, trump)
 
     pygame.quit()
 
